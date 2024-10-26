55246da1e9930e39042e1f492097b53747ca1f3b
York <jorgeglez.alva94@gmail.com>
2024-10-25 21:12:34 -0700

    add email service to send mails

---
+++ StratechAPI/Services/EmailService.cs
@@ -0,0 +1,46 @@
+﻿using MailKit.Security;
+using MimeKit;
+
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using StratechAPI.Model;
+
+namespace StratechAPI.Services
+{
+    public class EmailService
+    {
+        private readonly SmtpSettings _smtpSettings;
+
+        public EmailService(IOptions<SmtpSettings> smtpSettings)
+        {
+            _smtpSettings = smtpSettings.Value;
+        }
+
+        /** 
+        * Sends an email asynchronously to the specified recipient 
+        * with the provided subject and message content.
+        */
+        public async Task SendEmailAsync(string toEmail, string subject, string message)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Stratech API", _smtpSettings.Mail));
+            emailMessage.To.Add(new MailboxAddress("", toEmail));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart("html") { Text = message };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync(_smtpSettings.Server,_smtpSettings.Port, SecureSocketOptions.StartTls);
+                await client.AuthenticateAsync(_smtpSettings.Username, _smtpSettings.Password);
+                await client.SendAsync(emailMessage);
+                await client.DisconnectAsync(true);
+                client.Disconnect(true);
+            }
+        }
+
+    }
+}
