7c9d2822c61e977bfa4eacaee89e1b07b7432aa6
York <jorgeglez.alva94@gmail.com>
2024-10-25 21:21:23 -0700

    add RdsDbContent  model for database config

---
+++ StratechAPI/RdsDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StratechAPI.Model;
+
+namespace StratechAPI
+{
+    public class RdsDbContext: DbContext
+    {
+        public RdsDbContext(DbContextOptions<RdsDbContext> options) : base(options) { }
+        public DbSet<AudienceRecord> AudienceRecord { get; set; }
+
+    }
+}
