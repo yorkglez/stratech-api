20d064748d81cd98401c7a64c581f006ac488a5f
York <jorgeglez.alva94@gmail.com>
2024-10-25 21:04:15 -0700

    AudienceRecord model added

---
+++ StratechAPI/Model/AudienceRecord.cs
@@ -0,0 +1,13 @@
+﻿namespace StratechAPI.Model
+{
+    public class AudienceRecord
+    {
+        public int Id { get; set; } 
+        public DateTime Timestamp { get; set; }
+        public int Age { get; set; }
+        public string Genre { get; set; }
+        public string Mood { get; set; }
+        public int TotalAudience { get; set; }
+        public int DwellTimeAverage { get; set; }
+    }
+}
---
+++ StratechAPI/Model/SmtpSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace StratechAPI.Model
+{
+    public class SmtpSettings
+    {
+        public string Server { get; set; }
+        public int Port { get; set; }
+        public string Mail { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
